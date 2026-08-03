2873029b592cf9398308ff343c40359531f4a266
Manaswin Chaudhary <126452894+manaswin72@users.noreply.github.com>
2026-08-03 20:19:53 +0530

    Time: 0 ms (100%), Space: 71.9 MB (15.12%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+      // Input 
+int current_Max = nums[0];
+int global_Max = nums[0];
+
+for(int i =1 ; i < nums.size(); i++ ){
+    current_Max = max(nums[i], current_Max + nums [i]);
+    if(current_Max > global_Max){
+        global_Max = current_Max;
+    }
+}
+
+      //Kadanes's algorithm
+      return global_Max;
+
+      
+        
+    }
+};
