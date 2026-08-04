33d8624ef096411f7f0a6a1c7de750e4e68525b1
Manaswin Chaudhary <126452894+manaswin72@users.noreply.github.com>
2026-08-04 23:26:45 +0530

    Time: 0 ms (100%), Space: 8.8 MB (89.79%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,30 @@
+#include <stack>
+using namespace std;
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+
+        for (char val : s) {
+            if (val == '(' || val == '[' || val == '{') {
+                st.push(val);
+            } else {
+                if (st.empty()) {
+                    return false;
+                }
+
+                if (val == ')' && st.top() == '(') {
+                    st.pop();
+                } else if (val == ']' && st.top() == '[') {
+                    st.pop();
+                } else if (val == '}' && st.top() == '{') {
+                    st.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        return st.empty();
+    }
+};
