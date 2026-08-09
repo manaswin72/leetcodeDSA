a9836dd76292c87da55ba8fece7a07d1e3398fc8
Manaswin Chaudhary <126452894+manaswin72@users.noreply.github.com>
2026-08-09 21:26:35 +0530

    Time: 0 ms (100%), Space: 7.8 MB (82.37%) - LeetHub

---
+++ 3894-traffic-signal-color/3894-traffic-signal-color.cpp
@@ -0,0 +1,9 @@
+class Solution { 
+public:
+    string trafficSignal(int timer) {
+        if (timer > 30 && timer <= 90) return "Red";
+        else if (timer == 30) return "Orange";
+        else if (timer == 0) return "Green";
+        else return "Invalid";
+    }
+};
