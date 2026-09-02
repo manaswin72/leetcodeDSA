d3ad7e0579bee82431e1d8e6609e11a78654727b
Manaswin Chaudhary <126452894+manaswin72@users.noreply.github.com>
2026-09-02 19:35:19 +0530

    Time: 73 ms (18.18%), Space: 116.9 MB (15.15%) - LeetHub

---
+++ 0853-car-fleet/0853-car-fleet.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        
+        vector<pair<int, double>> cars;
+
+        for(int i = 0; i < position.size(); i++) {
+            double time = (double)(target - position[i]) / speed[i];
+            cars.push_back({position[i], time});
+        }
+
+        // Closest to target first
+        sort(cars.rbegin(), cars.rend());
+
+        int fleets = 0;
+        double maxTime = 0;
+
+        for(auto &car : cars) {
+            double time = car.second;
+
+            if(time > maxTime) {
+                fleets++;
+                maxTime = time;
+            }
+        }
+
+        return fleets;
+    }
+};
+//TC-O(nlogn) SC-O(n)
