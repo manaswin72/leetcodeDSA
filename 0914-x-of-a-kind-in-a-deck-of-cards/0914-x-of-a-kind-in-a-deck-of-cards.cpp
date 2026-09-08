60f1debe8d15ac7b894144337f96385571e76714
Manaswin Chaudhary <126452894+manaswin72@users.noreply.github.com>
2026-09-08 17:09:39 +0530

    Time: 0 ms (100%), Space: 21.4 MB (26.82%) - LeetHub

---
+++ 0914-x-of-a-kind-in-a-deck-of-cards/0914-x-of-a-kind-in-a-deck-of-cards.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool hasGroupsSizeX(vector<int>& deck) {
+       unordered_map<int,int>freq;
+       for(int card:deck){
+        freq[card]++;
+       } 
+       int g=0;
+       for(auto& [card,count] : freq){
+        g=gcd(g,count);
+       }
+       return g>1;
+    }
+};
